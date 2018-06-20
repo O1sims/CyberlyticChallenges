fa8c9a22b030641da997ee75cde2c5a1ca9ab948
Owen Sims <owen.sims@cyberlytic.com>
2018-06-20 17:37:48 +0100

    Add DELETE functionality

--- apiCreation/python/api/models/people.py
+++ apiCreation/python/api/models/people.py
@@ -1,8 +1,15 @@
 schema = {
+    'id': {
+        'type': 'integer',
+        'minlength': 1,
+        'maxlength': 100,
+        'required': True
+    },
     'firstname': {
         'type': 'string',
         'minlength': 1,
-        'maxlength': 10,
+        'maxlength': 100,
+        'required': True,
     },
     'lastname': {
         'type': 'string',
@@ -32,10 +39,11 @@ people = {
     'item_title': 'person',
     'additional_lookup': {
         'url': 'regex("[\w]+")',
-        'field': 'lastname'
+        'field': 'id'
     },
     'cache_control': 'max-age=10,must-revalidate',
     'cache_expires': 10,
-    'resource_methods': ['GET', 'POST'],
+    'resource_methods': ['GET', 'POST', 'DELETE'],
+    'item_methods': ['GET', 'DELETE'],
     'schema': schema
 }
--- apiCreation/python/api/settings.py
+++ apiCreation/python/api/settings.py
@@ -1,10 +1,13 @@
 import os
 from models.people import people
 
+
 DOMAIN = {
     'people': people
 }
 
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'DELETE']
 
 MONGO_HOST = os.environ.get('DB_HOSTNAME')
 MONGO_PORT = int(os.environ.get('DB_PORT'))
